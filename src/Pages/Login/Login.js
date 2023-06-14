7bfebcc50869cc67e120698e2417bd30d7292643
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2023-06-14 18:57:27 +0600

    Some Login Part

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h2></h2>
+    <div className="flex h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl p-8">
+        <div class="card-body">
+          <h2 class="text-center text-2xl font-bold">Login</h2>
+        </div>
+        <div className="divider font-bold">OR</div>
+        <button className="btn btn-outline w-75 mb-8">
+          Continue With Google
+        </button>
+      </div>
     </div>
   );
 };
