da3d8ea1c3fb855957524af5d6678b0886ec71f6
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2023-06-14 20:14:03 +0600

    Google Login Done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,10 @@
 import React from "react";
+import auth from "../../firebase.init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <div className="flex h-screen justify-center items-center">
       <div class="card w-96 bg-base-100 shadow-xl p-8">
@@ -8,9 +12,13 @@ const Login = () => {
           <h2 class="text-center text-2xl font-bold">Login</h2>
         </div>
         <div className="divider font-bold">OR</div>
-        <button className="btn btn-outline w-75 mb-8">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-outline w-75 mb-8"
+        >
           Continue With Google
         </button>
+        <h5>{loading}</h5>
       </div>
     </div>
   );
